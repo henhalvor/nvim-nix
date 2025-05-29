904852b3ffa1ffbfb25756c9df33182ade05082a
henhal <henhalvor@gmail.com>
2025-05-29 23:18:33 +0200

    relative path for symlink

--- flake.nix
+++ flake.nix
@@ -6,36 +6,25 @@
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    unstable,
-    ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-    pkgsUnstable = import unstable {inherit system;};
+  outputs = { self, nixpkgs, unstable, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      pkgsUnstable = import unstable { inherit system; };
 
-    lsp = import ./nix/lsp.nix {inherit pkgs pkgsUnstable;};
-    formatters = import ./nix/formatters.nix {inherit pkgs pkgsUnstable;};
-    tools = import ./nix/tools.nix {inherit pkgs pkgsUnstable;};
+      lsp = import ./nix/lsp.nix { inherit pkgs pkgsUnstable; };
+      formatters = import ./nix/formatters.nix { inherit pkgs pkgsUnstable; };
+      tools = import ./nix/tools.nix { inherit pkgs pkgsUnstable; };
 
-    allPackages = lsp ++ formatters ++ tools;
-  in {
-    homeManagerModules.default = {
-      config,
-      lib,
-      pkgs,
-      ...
-    }: {
-      home.packages = allPackages;
+      allPackages = lsp ++ formatters ++ tools;
+    in {
+      homeManagerModules.default = { config, lib, pkgs, ... }: {
+        home.packages = allPackages;
 
-      home.file.".config/nvim".source =
-        config.lib.file.mkOutOfStoreSymlink "${self}/config";
+        home.file.".config/nvim".source =
+          config.lib.file.mkOutOfStoreSymlink ./config;
+      };
+
+      devShells.${system}.default = pkgs.mkShell { packages = allPackages; };
     };
-
-    devShells.${system}.default = pkgs.mkShell {
-      packages = allPackages;
-    };
-  };
 }
