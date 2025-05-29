f2b1b476fe8e814b8a9294f6462a1e7069d7f9a0
henhal <henhalvor@gmail.com>
2025-05-29 23:12:42 +0200

    blink

--- config/lua/core/blink-cmp.lua
+++ config/lua/core/blink-cmp.lua
@@ -1,88 +1,89 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = {
-    'Kaiser-Yang/blink-cmp-avante',
-    {
-      'L3MON4D3/LuaSnip',
-      build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
-    },
-  },
-  version = '*',
+	"saghen/blink.cmp",
+	dependencies = {
+		"Kaiser-Yang/blink-cmp-avante",
+		{
+			"L3MON4D3/LuaSnip",
+			build = (function()
+				-- Build Step is needed for regex support in snippets.
+				-- This step is not supported in many windows environments.
+				-- Remove the below condition to re-enable on windows.
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				-- `friendly-snippets` contains a variety of premade snippets.
+				--    See the README about individual language/framework/plugin snippets:
+				--    https://github.com/rafamadriz/friendly-snippets
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
+			},
+		},
+	},
+	version = "*",
 
-  opts = {
-    keymap = { preset = 'default' },
+	opts = {
+		keymap = { preset = "default" },
 
-    appearance = {
-      nerd_font_variant = 'mono',
-    },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-    -- Add rounded border to the completion menu
-    completion = {
-      -- Make completion trigger faster
-      trigger = {
-        -- Enable showing on keywords for immediate triggering
-        show_on_keyword = true,
-        -- Prefetch completions when entering insert mode
-        prefetch_on_insert = true,
-      },
+		-- Add rounded border to the completion menu
+		completion = {
+			-- Make completion trigger faster
+			trigger = {
+				-- Enable showing on keywords for immediate triggering
+				show_on_keyword = true,
+				-- Prefetch completions when entering insert mode
+				prefetch_on_insert = true,
+			},
 
-      -- Configure menu appearance
-      menu = {
-        -- Set rounded border
-        border = 'rounded',
-        -- Ensure menu appears immediately
-        auto_show = true,
-      },
+			-- Configure menu appearance
+			menu = {
+				-- Set rounded border
+				border = "rounded",
+				-- Ensure menu appears immediately
+				auto_show = true,
+			},
 
-      -- Configure documentation window
-      documentation = {
-        auto_show = true,
-        window = {
-          border = 'rounded',
-        },
-      },
-    },
+			-- Configure documentation window
+			documentation = {
+				auto_show = true,
+				window = {
+					border = "rounded",
+				},
+			},
+		},
 
-    sources = {
-      -- Lower the minimum keyword length to trigger sources
-      min_keyword_length = 1,
-      default = { 'avante', 'lsp', 'path', 'snippets', 'buffer', 'codecompanion' },
-      providers = {
-        avante = {
-          module = 'blink-cmp-avante',
-          name = 'Avante',
-          opts = {
-            -- options for blink-cmp-avante
-          },
-        },
-      },
-    },
+		sources = {
+			-- Lower the minimum keyword length to trigger sources
+			min_keyword_length = 1,
+			default = { "avante", "lsp", "path", "snippets", "buffer" },
+			-- default = { 'avante', 'lsp', 'path', 'snippets', 'buffer', 'codecompanion' },
+			providers = {
+				avante = {
+					module = "blink-cmp-avante",
+					name = "Avante",
+					opts = {
+						-- options for blink-cmp-avante
+					},
+				},
+			},
+		},
 
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
-  },
-  opts_extend = { 'sources.default' },
-  -- config = function()
-  --   -- Setup luasnip
-  --   local luasnip = require 'luasnip'
-  --   luasnip.config.setup {}
-  -- end,
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
+	-- config = function()
+	--   -- Setup luasnip
+	--   local luasnip = require 'luasnip'
+	--   luasnip.config.setup {}
+	-- end,
 }
