9a0dff0b90c464e384d8c87a37c6f549b7be236f
henhal <henhalvor@gmail.com>
2025-07-28 23:53:13 +0200

    improve setup
    -- switch to yazi plugin for file management
    -- enable codecompanion
    -- add barbecue.nvim

---
+++ config/lua/plugins/archived/fyler.lua
@@ -0,0 +1,7 @@
+return {}
+-- return {
+--   "A7Lavinraj/fyler.nvim",
+--   dependencies = { "echasnovski/mini.icons" },
+--   branch = "stable",
+--   opts = {} -- check the default options in the README.md
+-- }
