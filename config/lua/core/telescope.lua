c86f20225ea4415a800a0cbd3aaa14452d0bb23c
henhal <henhalvor@gmail.com>
2025-09-27 21:27:39 +0200

    Add opencode plugin

--- config/lua/core/telescope.lua
+++ config/lua/core/telescope.lua
@@ -58,6 +58,9 @@ return {
 		local builtin = require("telescope.builtin")
 		-- vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+
+		vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { desc = "[S]earch [S]ymbols" })
+		vim.keymap.set("n", "<leader>ssw", builtin.lsp_workspace_symbols, { desc = "[S]earch [S]ymbols [W]orkspace" })
 		-- vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", "<leader>st", builtin.builtin, { desc = "[S]earch Select [T]elescope" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
