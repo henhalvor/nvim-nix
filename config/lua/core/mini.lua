ae136eb5efc2974b271ad24c023018f31bf09173
henhal <henhalvor@gmail.com>
2025-10-16 19:42:23 +0200

    misc
    statusline styling

--- config/lua/core/mini.lua
+++ config/lua/core/mini.lua
@@ -6,62 +6,62 @@ return {
 			"JoosepAlviste/nvim-ts-context-commentstring",
 		},
 		config = function()
-			--		Files
-			require("mini.files").setup({
-				vim.keymap.set(
-					"n",
-					"-",
-					"<CMD>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>",
-					{ desc = "Open parent directory" }
-				),
-
-				vim.keymap.set("n", "<Esc>", "<CMD>lua MiniFiles.close()<CR>", { desc = "Close MiniFiles" }),
-
-				content = {
-					-- Predicate for which file system entries to show
-					filter = nil,
-					-- What prefix to show to the left of file system entry
-					prefix = nil,
-					-- In which order to show file system entries
-					sort = nil,
-				},
-				-- Module mappings created only inside explorer
-				mappings = {
-					close = "q",
-					go_in = "l",
-					go_in_plus = "L",
-					go_out = "h",
-					go_out_plus = "H",
-					mark_goto = "'",
-					mark_set = "m",
-					reset = "<BS>",
-					reveal_cwd = "@",
-					show_help = "g?",
-					synchronize = "=",
-					trim_left = "<",
-					trim_right = ">",
-				},
-				-- General options
-				options = {
-					-- Whether to delete permanently or move into module-specific trash
-					permanent_delete = true,
-					-- Whether to use for editing directories
-					use_as_default_explorer = true,
-				},
-				-- Customization of explorer windows
-				windows = {
-					-- Maximum number of windows to show side by side
-					max_number = 4,
-					-- Whether to show preview of file/directory under cursor
-					preview = false,
-					-- Width of focused window
-					width_focus = 50,
-					-- Width of non-focused window
-					width_nofocus = 15,
-					-- Width of preview window
-					width_preview = 25,
-				},
-			})
+			--	Files
+			-- require("mini.files").setup({
+			-- 	vim.keymap.set(
+			-- 		"n",
+			-- 		"-",
+			-- 		"<CMD>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>",
+			-- 		{ desc = "Open parent directory" }
+			-- 	),
+			--
+			-- 	vim.keymap.set("n", "<Esc>", "<CMD>lua MiniFiles.close()<CR>", { desc = "Close MiniFiles" }),
+			--
+			-- 	content = {
+			-- 		-- Predicate for which file system entries to show
+			-- 		filter = nil,
+			-- 		-- What prefix to show to the left of file system entry
+			-- 		prefix = nil,
+			-- 		-- In which order to show file system entries
+			-- 		sort = nil,
+			-- 	},
+			-- 	-- Module mappings created only inside explorer
+			-- 	mappings = {
+			-- 		close = "q",
+			-- 		go_in = "l",
+			-- 		go_in_plus = "L",
+			-- 		go_out = "h",
+			-- 		go_out_plus = "H",
+			-- 		mark_goto = "'",
+			-- 		mark_set = "m",
+			-- 		reset = "<BS>",
+			-- 		reveal_cwd = "@",
+			-- 		show_help = "g?",
+			-- 		synchronize = "=",
+			-- 		trim_left = "<",
+			-- 		trim_right = ">",
+			-- 	},
+			-- 	-- General options
+			-- 	options = {
+			-- 		-- Whether to delete permanently or move into module-specific trash
+			-- 		permanent_delete = true,
+			-- 		-- Whether to use for editing directories
+			-- 		use_as_default_explorer = true,
+			-- 	},
+			-- 	-- Customization of explorer windows
+			-- 	windows = {
+			-- 		-- Maximum number of windows to show side by side
+			-- 		max_number = 4,
+			-- 		-- Whether to show preview of file/directory under cursor
+			-- 		preview = false,
+			-- 		-- Width of focused window
+			-- 		width_focus = 50,
+			-- 		-- Width of non-focused window
+			-- 		width_nofocus = 15,
+			-- 		-- Width of preview window
+			-- 		width_preview = 25,
+			-- 	},
+			-- })
 
 			require("mini.surround").setup({
 				-- Add custom surroundings to be used on top of builtin ones. For more
@@ -144,9 +144,51 @@ return {
 			-- default behavior. For example, here we set the section for
 			-- cursor location to LINE:COLUMN
 			---@diagnostic disable-next-line: duplicate-set-field
+			-- statusline.section_location = function()
+			-- 	return "%2l:%-2v"
+			-- end
+			--
+			-- Minimal sections
 			statusline.section_location = function()
 				return "%2l:%-2v"
 			end
+
+			-- Hide or simplify other sections
+			statusline.section_filename = function()
+				return "%f" -- Just filename without path/flags
+			end
+
+			statusline.section_fileinfo = function()
+				return "" -- Hide file info (encoding, type)
+			end
+
+			statusline.section_searchcount = function()
+				return "" -- Hide search count
+			end
+
+			statusline.section_git = function()
+				return "" -- Hide git info
+			end
+
+			statusline.section_diagnostics = function()
+				return "" -- Hide diagnostics
+			end
+
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				callback = function()
+					vim.api.nvim_set_hl(0, "MiniStatuslineFilename", { fg = "#ebdbb2", bold = true })
+					vim.api.nvim_set_hl(0, "MiniStatuslineDevinfo", { fg = "#ebdbb2" })
+					vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
+					vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
+				end,
+			})
+
+			vim.defer_fn(function()
+				vim.api.nvim_set_hl(0, "MiniStatuslineFilename", { fg = "#ebdbb2", bold = true })
+				vim.api.nvim_set_hl(0, "MiniStatuslineDevinfo", { fg = "#ebdbb2" })
+				vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
+				vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
+			end, 100)
 
 			require("mini.pairs").setup()
 			require("mini.diff").setup({
@@ -163,7 +205,7 @@ return {
 					style = "sign",
 
 					-- Signs used for hunks with 'sign' view
-					signs = { add = "▒", change = "▒", delete = "▒" },
+					-- signs = { add = "▒", change = "▒", delete = "▒" },
 
 					-- Priority of used visualization extmarks
 					priority = 199,
